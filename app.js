b1163cae7e5e2b2fa4a2dfab57bbe3947c86f502
Smrity Malik <93179331+Smrity-Malik@users.noreply.github.com>
2023-01-23 21:59:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("hamburger")=document.getElementById("hamburger")
+consolenavList = document.getElementById(nav-list)
+
+function toggleButton(){
+    navList.classList.toogle("show")
+
+}
+hamburgerButton.addEventListener("click", toogleButton)
